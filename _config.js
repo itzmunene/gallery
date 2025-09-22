035e3a957caf10880643b8534208dc2dcf6e8278
itzmunene <joemetha97@gmail.com>
2025-09-22 20:43:41 +0300

    config update

--- _config.js
+++ _config.js
@@ -1,9 +1,11 @@
 var config = {}
 
 config.mongoURI = {
-    production: 'mongodb+srv://munene:Ofof%402025@projectcluster1.9ycirzi.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://munene:Ofof%402025@projectcluster1.9ycirzi.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://munene:Ofof%402025@projectcluster1.9ycirzi.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+  production: 'mongodb+srv://munene:Ofof2025@projectcluster1.9ycirzi.mongodb.net/darkroom?retryWrites=true&w=majority',
+  development: 'mongodb+srv://munene:Ofof2025@projectcluster1.9ycirzi.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+  test: 'mongodb+srv://munene:Ofof2025@projectcluster1.9ycirzi.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+};
 
 module.exports = config;
+
+
