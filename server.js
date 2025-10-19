d54924b05866dd526cab82cba06e38ba2ae84929
itzmunene <joemetha97@gmail.com>
2025-10-20 01:49:27 +0300

    Updated server file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ console.log('Config:', config);
 const mongoURI = process.env.MONGO_URI_PRODUCTION || config.mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(mongodb_url, {
+mongoose.connect(mongodbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, (err) => {
